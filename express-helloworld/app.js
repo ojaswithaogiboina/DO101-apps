2b46feb8fd151d5c0ab5d58563e9f2601370b805
ojaswithaogiboina <ogiboinaojaswitha@gmail.com>
2023-11-25 12:10:28 +0530

    add /earth endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,8 +4,12 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
+app.get('/earth', function (req, res) {
+  res.send('Hello earth!\n');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
+
 
